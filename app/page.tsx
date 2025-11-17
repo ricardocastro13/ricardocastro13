5e5dbe06fcbbdcfd7166312bd8aa62b0b9c5c786
ricardocastro13 <a22402222@alunos.ulht.pt>
2025-11-17 23:42:52 +0000

    ff

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+import MagiaDoJSX from '@/components/MagiaDoJSX';
+
+export default function Page() {
+  return (
+    <div>
+      <h2>Interfaces Modernos</h2>
+      <p>Bem vindo à minha app em React e Next.js.</p>
+      <MagiaDoJSX />
+    </div>
+  )
+}
