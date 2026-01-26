f0f3d399b3c11de63e98a1e82503e729d799de83
ricardocastro13 <a22402222@alunos.ulht.pt>
2026-01-26 17:15:14 +0000

    a

--- app/jogadores/page.tsx
+++ app/jogadores/page.tsx
@@ -4,31 +4,32 @@ import { useEffect, useState } from "react"
 import Jogador from "@/components/Jogador/Jogador"
 import JogadoresPorto from "@/app/data/jogadores.json"
 import { JogadorInterface } from "@/models/interfaces"
-import Link from 'next/link'
+import Link from "next/link"
 
 export default function Page() {
-  
-  //estados para os filtros
   const [posicao, setPosicao] = useState("todos")
   const [estatistica, setEstatistica] = useState("MenosGolos")
   const [pais, setPais] = useState("todos")
   const [jogadores, setJogadores] = useState<JogadorInterface[]>([])
 
-  //EFEITOS
+  const [gosto, setGosto] = useState(0)
+
   useEffect(() => {
     setJogadores(JogadoresPorto)
   }, [])
-  const [gosto, setGosto ] = useState(()=>{
 
-        const gostoStored = localStorage.getItem('gosto')
-        return gostoStored? Number(gostoStored) : 0
-     })
- function aumentargosto(){
-        setGosto(gosto+1)
-     }
+  useEffect(() => {
+    const gostoStored = localStorage.getItem("gosto")
+    setGosto(gostoStored ? Number(gostoStored) : 0)
+  }, [])
 
+  useEffect(() => {
+    localStorage.setItem("gosto", String(gosto))
+  }, [gosto])
 
-
+  function aumentargosto() {
+    setGosto((x) => x + 1)
+  }
 
   return (
     <>
@@ -36,35 +37,22 @@ export default function Page() {
 
       {JogadoresPorto.length == 0 && <p>Nenhum jogador encontrado</p>}
 
-     
+      {JogadoresPorto.map((a, index) => (
+        <div key={a.nome}>
+          <Link href={`/jogadores/${index}`}>
+            <Jogador
+              nome={a.nome}
+              idade={a.idade}
+              golos={a.golos}
+              assistencias={a.assistencias}
+              nacionalidade={a.nacionalidade}
+            />
+          </Link>
 
-      {JogadoresPorto.map((a,index) => (
-        <div>
-        <Link href={`/jogadores/${index}`}>
-        <Jogador
-          key={a.nome}
-          nome={a.nome}
-          idade={a.idade}
-          golos={a.golos}
-          assistencias={a.assistencias}
-          nacionalidade={a.nacionalidade}
-          
-        />
-        
-        </Link>
-         <p>O quanto eu gosto do jogador: {gosto}</p>
-
-         <button onClick={aumentargosto}
-
-         > gosto
-
-         </button>
+          <p>O quanto eu gosto do jogador: {gosto}</p>
+          <button onClick={aumentargosto}>gosto</button>
         </div>
-        
       ))}
-
-
-      
     </>
   )
-} 
+}
