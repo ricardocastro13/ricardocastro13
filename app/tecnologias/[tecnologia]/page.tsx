823884651e84fd74c1708b1dce91ad6fa867ce5d
ricardocastro13 <a22402222@alunos.ulht.pt>
2026-01-26 17:07:07 +0000

    s

---
+++ app/tecnologias/[tecnologia]/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+
+    return (
+      <div>
+        <h1>Detalhes da Tecnologia</h1>
+        <p>Aqui estarão os detalhes da tecnologia selecionada.</p>
+      </div>
+    );
+}
+
+  
+  
